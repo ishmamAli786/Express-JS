d1eac13dfd798ea92e8559caf1e8b7c59d722f71
Ishmam Ali <56753433+ishmamAli786@users.noreply.github.com>
2020-10-13 22:09:08 +0500

    Routing Using Router

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-//// init code
-require('dotenv').config();
 const express=require('express');
-const morgan=require('morgan');
-const cors =require('cors');
 const app=express();
-const port=process.env.PORT;
-const database=require('./database');
-const userController=require('./controlars/user');
-///middleware setup
-app.use(morgan('dev'));
-app.use(cors());
-app.use('/api/user', userController);
+const courses=require('./courses');
+app.use('/courses',courses);
 
-///default route
-app.all(
-    '/',(req,res)=>{
-        return res.json({
-            status:true,
-            message:'index page is working.....'
-        });
-    }
-);
-////start server
-app.listen(port,()=>{
-    console.log('server running is on:'+port);
-})
+
+
+app.get('/',(req,res)=>{
+    res.send('welcome to express tutorial')
+});
+app.listen(80);
